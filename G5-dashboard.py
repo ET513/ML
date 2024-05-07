686601a2aca5c03b754bfaf12da7e3bf353aa00c
ET513 <110839431+ET513@users.noreply.github.com>
2024-05-07 21:44:12 +0800

    Update G5-dashboard.py

--- G5-dashboard.py
+++ G5-dashboard.py
@@ -700,7 +700,7 @@ scores = {
     'GMM1': gmm1_silhouette,
     'GMM2': gmm2_silhouette,
     'DBSCAN1': dbscan1_silhouette,
-    'DBSCAN2': dbscan2_silhouette，
+    'DBSCAN2': dbscan2_silhouette,
     'KMedoidsMin1': kmedoidsmin1_silhouette,
     'KMedoidsMin2': kmedoidsmin2_silhouette
 }
